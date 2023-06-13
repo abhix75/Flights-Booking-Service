999b9f96628c0d5c951cac904ef5731b355244d0
Abhijit Mishra <abhijitmishraak10@gmail.com>
2023-06-13 16:57:21 +0530

    ADDDED THE MVC STRUCTURE

---
+++ src/repositories/booking-repository.js
@@ -0,0 +1,14 @@
+const BookingRepository = require('./crud-repository');
+
+const {Booking}= require('../models');
+
+
+class BookingRepository extends CrudRepository
+{
+     constructor()
+     {
+        super(Booking);
+     }
+}
+
+module.exports = BookingRepository;
